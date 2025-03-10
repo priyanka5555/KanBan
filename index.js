b4eb65e28f97582b97653e68f6e8785a0111e2af
Priyanka <priyankasharma080396@gmail.com>
2025-03-10 09:10:57 -0400

    drag functionality added - cards can now be dragged on other boards

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const itemBtn = document.getElementsByClassName("itemBtn")[2]
+const itemBtn = document.getElementsByClassName("itemBtn")
 const inputDiv = document.getElementById("inputDiv")
 const task = document.getElementById("taskName")
-const todoBoard = document.getElementsByClassName("todo-board")
+const todoBoard = document.getElementById("todo-board")
+const item = document.getElementsByClassName("item")
 
-itemBtn.addEventListener("click", function () {
+
+function attachDragEvents(target){
+    target.addEventListener("dragstart", () => {
+        target.classList.add("flying")  //added flying class to identify element which is dragging over board
+    })
+
+    target.addEventListener("dragend", () => {
+        target.classList.remove("flying")
+    })
+}
+
+// add item button click functionality - overlay effect when button is click and input box appears 
+itemBtn[2].addEventListener("click", function () {
 
     const overlay = document.createElement("div");
     overlay.classList.add("inputStyling");
@@ -12,20 +25,64 @@ itemBtn.addEventListener("click", function () {
     task.style.display = "block"
     // overlay.appendChild(task)
 
+    // cancel button click functionality inside add item  
     const cancelBtn = document.getElementsByClassName("itemBtn")[1]
-    cancelBtn.onclick = function(){
-        task.style.display="none"
-        overlay.style.display ="none"
-    }
+    cancelBtn.addEventListener("click", function () {
+        task.style.display = "none"
+        overlay.style.display = "none"
+    })
 
-    // const addTask = document.getElementsByClassName("itemBtn")[0]
-    // addTask.onclick = function(){
-    //     const newTask = document.createElement("p")
-    //     newTask.classList.add("item")
-    //     newTask.setAttribute('draggable','true')
-    //     newTask.innerHTML = taskInput.
-    //     todoBoard.appendChild(newTask)
-    // }
+    // add task button click functionality inside add item  
+    const todoTask = document.getElementsByClassName("itemBtn")[0]
 
+    todoTask.addEventListener("click", function () {
+
+        const userInput = document.getElementById("taskInput").value
+        const newCard = document.createElement("p")
+        newCard.classList.add("item")
+        newCard.setAttribute("draggable", "true")
+        newCard.innerText = userInput
+        todoBoard.appendChild(newCard)
+        document.getElementById("taskInput").value = ""
+        cancelBtn.click()
+
+        attachDragEvents(newCard)
+
+        if (userInput == "") {
+            newCard.remove("item")
+            return
+        }
+    })
+
+    
+})
+
+//drag elements functionality
+const todo_draggable_item = document.querySelectorAll(".item")  //Nodelist
+const progressBoard = document.getElementById("progress-board")
+const completeBoard = document.getElementById("complete-board")
+const allboard = document.querySelectorAll(".board")
+
+todo_draggable_item.forEach(function (drag_item) {  //loop nodelist
+    // drag_item.addEventListener("dragstart", function () {
+    //     drag_item.classList.add("flying") //added flying class to identify element which is dragging over board
+    //     console.log(drag_item.innerHTML + " is draggable now")
+    // })
+
+    // drag_item.addEventListener("dragend", () => {
+    //     drag_item.classList.remove("flying")
+    // }) 
+    attachDragEvents(drag_item)
+
+    allboard.forEach((board) => {      //get all board name
+    board.addEventListener("dragover", () => {
+
+            const flyingElement = document.querySelector(".flying");
+            console.log("on " + board.id + " dragged this item : " + flyingElement.innerHTML)
+            board.appendChild(flyingElement)
+            
+        })
+
+    })
 
 })
